c720ef94cf324c0bb4ec51efa2325da7e680ac42
mbozecki <56350662+mbozecki@users.noreply.github.com>
2020-11-02 16:10:04 +0100

    lista2

---
+++ Zad6_MikolajBozecki/Zad6MikolajBozecki.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+template <int length,int dim> struct hipervolume
+{
+	enum {v=length*hipervolume<length,dim-1>::v};
+};
+
+template <int length> struct hipervolume <length ,0>
+{
+	enum { v = 1 };
+};
+
+template <int length> struct hipervolume <length, -1>
+{
+	enum {v=-1};
+};
+
+template <int dim> struct hipervolume <-1, dim>
+{
+	enum { v = -1 };
+};
+
+template <> struct hipervolume <-1, -1>
+{
+	enum { v = -1 };
+};
+
+int main()
+{
+	cout << "6^3= ";
+	cout << hipervolume<6, 3>::v << endl;
+	cout << hipervolume<6, -1>::v << endl;
+	cout << hipervolume<-1, 3>::v << endl;
+	cout << hipervolume<-1, -1>::v << endl;
+}
